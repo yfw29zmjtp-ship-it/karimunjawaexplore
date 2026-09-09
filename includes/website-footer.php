1b871cccadaf27c08aa5c8bc22a32cb1c212d728
Arif Narayana <alfiannaila.517@gmail.com>
2026-09-09 12:49:06 +0700

    Chat widget: use company logo as avatar, replace fab emoji with elegant SVG icon

--- includes/website-footer.php
+++ includes/website-footer.php
@@ -36,7 +36,13 @@
             <div class="we-chat-panel" id="weChatPanel">
                 <div class="we-chat-header">
                     <div class="we-chat-header-info">
-                        <span class="we-chat-avatar">🌊</span>
+                        <span class="we-chat-avatar">
+                            <?php if ($weLogoSrc): ?>
+                                <img src="<?php echo htmlspecialchars($weLogoSrc); ?>" alt="<?php echo htmlspecialchars($weCompanyName); ?>">
+                            <?php else: ?>
+                                🌊
+                            <?php endif; ?>
+                        </span>
                         <div>
                             <div class="we-chat-title"><?php echo htmlspecialchars($weCompanyName); ?></div>
                             <div class="we-chat-status">Biasanya membalas cepat via WhatsApp</div>
@@ -55,7 +61,9 @@
                 </div>
             </div>
             <button type="button" class="we-chat-fab" id="weChatFab" onclick="weChatToggle()" aria-label="Chat WhatsApp">
-                <span class="we-chat-fab-icon">💬</span>
+                <svg class="we-chat-fab-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M12 3C7.03 3 3 6.58 3 11c0 2.39 1.19 4.53 3.08 6.02-.1.98-.42 2.28-1.28 3.48-.14.2.02.47.27.44 1.7-.2 3.13-.9 4.2-1.58.86.24 1.78.37 2.73.37 4.97 0 9-3.58 9-8s-4.03-8-9-8z" fill="currentColor"/>
+                </svg>
             </button>
         </div>
 
